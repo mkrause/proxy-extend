ecdad2835032e31346dea8326393847f074e4ee4
mkrause <maikelkrause@gmail.com>
2020-03-07 22:59:51 +0100

    Remove `type` field from package.json.

---
+++ babel.config.cjs
@@ -0,0 +1,33 @@
+
+const target = process.env.BABEL_ENV || 'esm';
+
+module.exports = babel => {
+    //babel.cache(false);
+    
+    return {
+        presets: [
+            ['@babel/env', {
+                targets: {
+                    browsers: [
+                        'node 8.9', // Support Node v8.9 LTS (Carbon)
+                        '>0.1%',
+                        'not dead',
+                        'not OperaMini all',
+                        'not IE < 11',
+                        'last 2 Edge versions',
+                    ],
+                },
+                
+                // Do not include polyfills automatically. Leave it up to the consumer to include the right polyfills
+                // for their required environment.
+                useBuiltIns: false,
+                
+                // Whether to transpile modules
+                modules: target === 'cjs' ? 'commonjs' : false,
+            }],
+        ],
+        plugins: [
+        ],
+        sourceMaps: 'inline',
+    };
+};
