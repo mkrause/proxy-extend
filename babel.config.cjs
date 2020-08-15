f71ef0c66cb029f87c6c4be139a1c5c2890d3de3
mkrause <maikelkrause@gmail.com>
2020-08-15 03:26:44 +0200

    Upgrade dependencies.

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,36 +1,31 @@
 
 const target = process.env.BABEL_ENV || 'esm';
 
-module.exports = babel => {
-    babel.cache(false);
-    
-    return {
-        presets: [
-            ['@babel/env', {
-                targets: {
-                    browsers: [
-                        'node 10.13', // Support Node v10.13 LTS (Dubnium) or higher
-                        'last 2 Chrome versions',
-                        'last 2 Firefox versions',
-                        'last 2 Safari versions',
-                        'last 2 Edge versions',
-                        '>0.1%',
-                        'not dead',
-                        'not OperaMini all',
-                        'not IE < 11',
-                    ],
-                },
-                
-                // Do not include polyfills automatically. Leave it up to the consumer to include the right polyfills
-                // for their required environment.
-                useBuiltIns: false,
-                
-                // Whether to transpile modules
-                modules: target === 'cjs' ? 'commonjs' : false,
-            }],
-        ],
-        plugins: [
-        ],
-        sourceMaps: 'inline',
-    };
+module.exports = {
+    presets: [
+        ['@babel/env', {
+            targets: {
+                browsers: [
+                    'node 10.13', // Support Node v10.13 LTS (Dubnium) or higher
+                    'last 2 Chrome versions',
+                    'last 2 Firefox versions',
+                    'last 2 Safari versions',
+                    'last 2 Edge versions',
+                    '>0.1%',
+                    'not dead',
+                    'not OperaMini all',
+                    'not IE < 11',
+                ],
+            },
+            
+            // Do not include polyfills automatically. Leave it up to the consumer to include the right polyfills
+            // for their required environment.
+            useBuiltIns: false,
+            
+            // Whether to transpile modules
+            modules: target === 'cjs' ? 'commonjs' : false,
+        }],
+    ],
+    plugins: [
+    ],
 };
