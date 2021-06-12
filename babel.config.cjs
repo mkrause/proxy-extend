debb96caf2b7821fbddcafe5f26c8a4e71f55031
maikelkrause <maikel.krause@fortanix.com>
2021-06-13 01:29:57 +0200

    Upgrade dependencies, drop support for Node 10 + IE 11.

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,22 +2,15 @@
 const target = process.env.BABEL_ENV || 'esm';
 
 module.exports = {
+    targets: {
+        browsers: [
+            'defaults',
+            'not IE 11',
+            'node 12.13', // Support Node v12.13 LTS (Erbium) or higher
+        ],
+    },
     presets: [
         ['@babel/env', {
-            targets: {
-                browsers: [
-                    'node 10.13', // Support Node v10.13 LTS (Dubnium) or higher
-                    'last 2 Chrome versions',
-                    'last 2 Firefox versions',
-                    'last 2 Safari versions',
-                    'last 2 Edge versions',
-                    '>0.1%',
-                    'not dead',
-                    'not OperaMini all',
-                    'not IE < 11',
-                ],
-            },
-            
             // Do not include polyfills automatically. Leave it up to the consumer to include the right polyfills
             // for their required environment.
             useBuiltIns: false,
